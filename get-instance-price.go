f4c4745470f302fcf74fc730f7aafaaa98de088c
claredavies <clare.davies@abo.fi>
2023-10-03 16:27:59 +0100

    Add get-instance-price package

---
+++ get-instance-price.go
@@ -0,0 +1,12 @@
+package get_instance_price
+
+// comment
+var Version = "1.0"
+
+func Add(lhs int, rhs int) int {
+	return lhs + rhs
+}
+
+func Subtract(lhs int, rhs int) int {
+	return lhs - rhs
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/claredavies/get-instance-price
+
+go 1.21.1
